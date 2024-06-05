4863b1e5e496baf12ede5b4f400285a407ba7006
Stefan Probst <stefanprobst@posteo.de>
2024-06-05 07:24:11 +0200

    chore: make db slow

--- data/db.ts
+++ data/db.ts
@@ -1,7 +1,10 @@
 import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 
+import { wait } from "@acdh-oeaw/lib";
+
 export async function getFruits() {
+  await wait(5000);
   const filePath = join(process.cwd(), "data", "fruits.json");
   const fileContent = await readFile(filePath, { encoding: "utf-8" });
   const fruits = JSON.parse(fileContent) as Array<string>;
