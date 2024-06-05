6019eeb7a1ea47c1e1937720fbc8164931a1863a
Stefan Probst <stefanprobst@posteo.de>
2024-06-05 07:47:44 +0200

    feat: add interactive form pt.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { getFruits } from "@/data/db";
-import { Suspense } from "react";
+import { FormEvent, Suspense } from "react";
 
 export default function Home() {
   return (
@@ -15,11 +15,40 @@ export default function Home() {
 async function Fruits() {
   const fruits = await getFruits();
 
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    const formData = new FormData(event.currentTarget);
+    const fruits = formData.getAll("fruits");
+    alert(JSON.stringify(fruits, null, 2));
+  }
+
   return (
-    <ul role="list">
-      {fruits.map((fruit) => {
-        return <li key={fruit}>{fruit}</li>;
-      })}
-    </ul>
+    <form onSubmit={onSubmit}>
+      <ul role="list">
+        {fruits.map((fruit) => {
+          return (
+            <li key={fruit}>
+              <CheckBox name="fruits" value={fruit} />
+            </li>
+          );
+        })}
+      </ul>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+interface CheckBoxProps {
+  name: string;
+  value: string;
+}
+
+function CheckBox(props: CheckBoxProps) {
+  const { name, value } = props;
+
+  return (
+    <label>
+      <input type="checkbox" name={name} value={value} />
+      <span>{value}</span>
+    </label>
   );
 }
