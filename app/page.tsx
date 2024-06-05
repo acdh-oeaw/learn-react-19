ff9b50e8358a85dfbf611a08a064ccb13283d4c3
Stefan Probst <stefanprobst@posteo.de>
2024-06-05 07:25:15 +0200

    refactor: move fruits to separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import { getFruits } from "@/data/db";
 
-export default async function Home() {
-  const fruits = await getFruits();
-
+export default function Home() {
   return (
     <main>
       <div>All the fruits!</div>
-      <ul role="list">
-        {fruits.map((fruit) => {
-          return <li key={fruit}>{fruit}</li>;
-        })}
-      </ul>
+      <Fruits />
     </main>
+  );
+}
+
+async function Fruits() {
+  const fruits = await getFruits();
+
+  return (
+    <ul role="list">
+      {fruits.map((fruit) => {
+        return <li key={fruit}>{fruit}</li>;
+      })}
+    </ul>
   );
 }
